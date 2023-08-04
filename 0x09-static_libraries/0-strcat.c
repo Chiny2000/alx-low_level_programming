c10c2847a8f9cf86822ea9d3dd6e92b703b9f397
Chiny2000 <enocho460@gmail.com>
2023-08-03 21:55:17 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,31 +1,25 @@
 #include "main.h"
- /**
-*_strcat - concatenates two strings
-*@dest: input value
-*@src: input value
-*
-*Return: void
-*/
-char *_strcat(char *dest, char *src)
+
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
 {
-int i;
-int j;
+	int l = 0;
+	int x = 0;
 
 
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-
-
-dest[i] = '\0';
-return (dest);
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
 }
