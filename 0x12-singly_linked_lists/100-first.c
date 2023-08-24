30e075534afa43d12a6ee30bf7d63f9c5a7d3bf1
Chiny2000 <enocho460@gmail.com>
2023-08-24 15:09:08 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void startupFunction(void) __attribute__((constructor));
+
+/**
+ * startupFunction -Displays output prior to the 
+ * execution of the main function.
+ *
+ */
+void startupFunction(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
