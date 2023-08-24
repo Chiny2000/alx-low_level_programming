a147aa125c325cf5a0d49afc57f59aff7bdf58d3
Chiny2000 <enocho460@gmail.com>
2023-08-24 14:14:04 -0700

    1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - Provides the count of elements 
+ * present within the linked list
+ *
+ * @h: starting point of the list
+ *
+ * Return: the number of nodes
+ */
+
+int list_len(const list_t *h)
+{
+const list_t *tmp;
+int count = 0;
+tmp = h;
+
+while (tmp != NULL)
+{
+count++;
+tmp = tmp->next;
+}
+
+return (count);
+}
