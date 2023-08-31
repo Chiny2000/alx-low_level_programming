05821ed52a32d7c9a233732a66846bda1bca08a5
Chiny2000 <enocho460@gmail.com>
2023-08-31 16:10:40 -0700

    6. Endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * get_endianness - Determines whether a system follows little-endian or big-endian byte order
+ * get_endianness - Determines whether a system follows
+ * little-endian or big-endian byte order
  * Return: 0 for big, 1 for little
  */
 int get_endianness(void)
